0410bc00db39aa8e3b44d3dd9a6fa26a6f5a4dbb
Isaque Lopes <isaque.lopes047@gmail.com>
2023-02-08 16:38:51 -0300

    add

--- connect/mysql.js
+++ connect/mysql.js
@@ -1,7 +1,6 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    connectionLimit : 10000,
     host: 'sql883.main-hosting.eu',
     port: 3306,
     user: 'u936282438_roottb2023',
